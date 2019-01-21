06a7e9fa7d1c398d65af2851eee71d3d3cff8645
codenamekola <kadebayo@calminfotech.com>
2019-01-21 15:33:01 +0100

    spaced user list

--- public/js/chat.js
+++ public/js/chat.js
@@ -58,12 +58,12 @@ socket.on('connect', function () {
     //listen for new users joining the chat room
     socket.on('updateUserList', function(users){
         console.log('Users',users);
-        var ol = jQuery('<ol class="list-group"></ol>');
+        var p = jQuery('<p></p>');
         users.forEach(function(user){
-            ol.append(jQuery('<li class="list-group-item"></li>').text(user));
+            p.append(jQuery('<code></code>').text(user).append(' '));
         });
 
-        jQuery('#chat-users').html(ol);
+        jQuery('#chat-users').html(p);
     });
     //listen for new location messages from server
     socket.on('newLocationMessage', function(message){
